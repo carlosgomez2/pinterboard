d44136d04705594f7203c022b1dd45ea36b7809e
Carlos Gomez <carlosgomez.deb@gmail.com>
2017-10-31 13:06:46 -0600

    included minitest-reporters

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 
+# Gems added
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'simple_form'
 gem 'devise'
@@ -21,6 +22,10 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
   gem 'byebug'
+end
+
+group :test do
+  gem 'minitest-reporters'
 end
 
 group :development do
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,15 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+
+# Improved Minitest output (color and progress bar)
+require "minitest/reporters"
+Minitest::Reporters.use!(
+  Minitest::Reporters::ProgressReporter.new(:color => true),
+  Minitest::Reporters::MeanTimeReporter.new(:color => true),
+  ENV,
+  Minitest.backtrace_filter)
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
